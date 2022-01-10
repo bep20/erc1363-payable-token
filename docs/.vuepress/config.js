523cc43b750a6ce4cc58648af0f52c825b67bfb9
bep20 <83491576+bep20@users.noreply.github.com>
2022-01-10 20:10:26 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,14 +12,14 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { property: 'og:type', content: 'website' }],
-    ['meta', { property: 'og:url', content: 'https://vittominacori.github.io/erc1363-payable-token' }],
-    ['meta', { property: 'og:image', content: 'https://vittominacori.github.io/erc1363-payable-token/assets/images/erc1363-payable-token.jpg' }], // eslint-disable-line max-len
+    ['meta', { property: 'og:url', content: 'https://BEP20.github.io/erc1363-payable-token' }],
+    ['meta', { property: 'og:image', content: 'https://BEP20.github.io/erc1363-payable-token/assets/images/erc1363-payable-token.jpg' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
-    ['meta', { property: 'twitter:image', content: 'https://vittominacori.github.io/erc1363-payable-token/assets/images/erc1363-payable-token.jpg' }], // eslint-disable-line max-len
+    ['meta', { property: 'twitter:image', content: 'https://BEP20.github.io/erc1363-payable-token/assets/images/erc1363-payable-token.jpg' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:title', content: 'ERC-1363 Payable Token | An ERC-20 token that can be used for payments' }],
   ],
   themeConfig: {
-    repo: 'vittominacori/erc1363-payable-token',
+    repo: 'BEP20/erc1363-payable-token',
     sidebar: 'auto',
   },
 };
